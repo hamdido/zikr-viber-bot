49f800f1181bbdfa9e74fd6538315e4d04e6c8b7
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-02-14 23:13:01 +0200

    fix: change pg connection details

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const postgres = require('postgres')
 
 console.log("Connecting to",process.env.DATABASE_URL)
-const sql = postgres(process.env.DATABASE_URL, {ssl:true})
+const sql = postgres(process.env.DATABASE_URL, {ssl: { rejectUnauthorized: false }})
 
 module.exports = sql
