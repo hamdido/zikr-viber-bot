32b1cea32503e61242aad5095144bcfe3bfd854c
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-02-14 23:08:50 +0200

    fix: update pg connection

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const postgres = require('postgres')
 
 console.log("Connecting to",process.env.DATABASE_URL)
-const sql = postgres(process.env.DATABASE_URL)
+const sql = postgres(process.env.DATABASE_URL, {ssl:true})
 
 module.exports = sql
